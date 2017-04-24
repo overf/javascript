42411652e0cdfbd5f9f4517a21fc1fa3ae2ad5d6
overf <overfilling@naver.com>
2017-04-24 17:54:21 +0900

    2

---
+++ assets/js/doc.js
@@ -0,0 +1,22 @@
+//1.변수,함수,데이터타입(string,array...),연산자,전역변수,지역변수,매개변수,스코프,함수활용
+
+/* var */
+/*
+
+*/
+/*
+
+*/
+/*
+
+*/
+/* function */
+
+
+
+
+
+/* selector */
+document.getElementById('id');
+document.getElementsByTagName('name');
+document.getElementsByClassName('name');
